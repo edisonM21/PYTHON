df1ab2653dbe3f277fa8c8563f77a106c4650365
401 <401@academicos.cesde.edu.co>
2022-03-07 14:52:00 -0500

    clase de python 07/03/2022

---
+++ main.py
@@ -0,0 +1,48 @@
+#logica con python
+#edad=int(input("ingrese su edad: "))
+#print(edad)
+#bandera=True
+
+#if(edad>=18 or bandera==True):
+#    print("Bienvenido a la chismosa")
+
+#else:
+#  print("salgase pa la mierdaa")
+'''nivelAgua=float(input("Digite el nivel del agua: "))
+if(nivelAgua<200 and nivelAgua>0):
+    print("La represa tiene poca agua")
+elif(nivelAgua>=200 and nivelAgua<450):
+    print("La represa esta estable")
+elif(nivelAgua>450):
+    print("Danger la represa va a estallar")
+else:
+    print("Digito un nivel invalido")'''
+
+
+
+'''Clima=input("Digite un mes del año: ").lower()
+if(Clima=="enero" or Clima=="febrero" or Clima=="marzo"):
+    print("Estas en invierno")
+elif(Clima=="abril" or Clima=="mayo" or Clima=="junio"):
+    print("Estas en verano")
+elif(Clima=="julio" or Clima=="agosto" or Clima=="septiembre"):
+    print("Estas en otoño")
+elif(Clima=="octubre" or Clima=="noviembre" or Clima=="diciembre"):
+    print("Estas en primavera")
+else:
+    print("Digitaste un mes equivocado")'''
+
+'''Etapavida=float(input("Ingrese su edad: "))
+if(Etapavida<=14 and Etapavida>=0):
+    print("Eres un niñoooooooooo")
+elif(Etapavida>14 and Etapavida <=28):
+    print("Eres un joven")
+elif(Etapavida>28 and Etapavida <60):
+    print("Eres un adultooooo")
+elif(Etapavida>=60):
+    print("Eres un viejo verdeeeeee")
+else:
+    print("ingresaste la edad mal bobo hpta")'''
+
+parametro=True
+print("El parametro es verdadero")if parametro==True else print("El prametro es falso")
